152ea9448f87076ef5ae12ec6f858065f072afba
DevPiccinatto <tiagopiccinatto@outlook.com>
2023-02-02 01:26:25 -0300

    Config Contexto e Adicionando Metodos

--- Catalogo.Domain/Interface/IProdutoRepository.cs
+++ Catalogo.Domain/Interface/IProdutoRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Catalogo.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace Catalogo.Domain.Interface
 {
     public interface IProdutoRepository
     {
+        public Produto Salvar(Produto produto);
+
+        public List<Produto> GetProdutos();
+
+
     }
 }
--- Catalogo.Infra/Repositorio/ProdutoRepository.cs
+++ Catalogo.Infra/Repositorio/ProdutoRepository.cs
@@ -1,9 +1,29 @@
-﻿using Catalogo.Domain.Interface;
+﻿using Catalogo.Domain.Entities;
+using Catalogo.Domain.Interface;
+using Catalogo.Infra.Context;
 
 namespace Catalogo.Infra.Repositorio
 {
+    
+
     public class ProdutoRepository : IProdutoRepository
     {
+        private readonly CatalogoContext _catalogoContext;
+        public ProdutoRepository(CatalogoContext catalogoContext)
+        {
+            _catalogoContext = catalogoContext;
+        }
 
+        public List<Produto> GetProdutos()
+        {
+          return _catalogoContext.produtos.ToList();
+        }
+
+        public Produto Salvar(Produto produto)
+        {
+            _catalogoContext.produtos.Add(produto);
+            _catalogoContext.SaveChanges();
+            return produto;
+        }
     }
 }
